c78bf57d840fef6161646032c6070a2ab66ade95
wb-dgq270700 <wb-dgq270700@alibaba-inc.com>
2017-06-13 17:01:56 +0800

    gulp动态启动方式服务器入口更改为./bin/www-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var webpackProdutionConfig = require('./gulp-webpack.js')
 gulp.task('nodemon', function(cb) {
     var started = false;
     return nodemon({
-        script: './bin/www'
+        script: './bin/www-gulp'
     }).on('start', function() {
         // to avoid nodemon being started multiple times
         // thanks @matthisk
@@ -48,25 +48,3 @@ gulp.task('vue', function() {
 gulp.task('default', ['browser-sync'], function() {
     gulp.watch('front/src/**/*.*', ['vue'])
 })
-
-// gulp启动webpack-dev-server
-/**
- * 使用测试配置打包，启动hot dev server
- */
-// gulp.task('webpack-dev', ['concat-lib'], function() {
-//     var config = Object.create(webpackConfigDev);
-//     //这两项配置原本是在webpack.config.dev.js里边配置，可是通过gulp启动devserver，那种配置无效，只能在此处写入
-//     //官网的解释是webpack-dev-server没有权限读取webpack的配置
-//     config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server");
-//     config.plugins.push(new webpack.HotModuleReplacementPlugin());
-//     var compiler = webpack(config);
-//     var server = new WebpackDevServer(compiler, {
-//         contentBase: "../",
-//         publicPath: "/release/",
-//         hot: true,
-//         compress: false,
-//         stats: { colors: true }
-//     });
-//     server.listen(8080, "localhost", function() {});
-//     // server.close();
-// });
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const router = require('koa-router')()
 var ConnextMongodb = require('../connect/connect.js')
 var cnt = new ConnextMongodb();
 
-// router.get('/', async(ctx, next) => {
-//     await ctx.render('index', {
-//         title: 'Hello Koa 2!'
-//     })
-// })
+router.get('/', async(ctx, next) => {
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
+})
 
 router.get('/string', async(ctx, next) => {
     ctx.body = 'koa2 string'
