683e2d55b624c92c72f745867aab258d44267f8f
shanguoqiang <guoqiang.shan@healthbok.com>
2017-07-24 16:15:18 +0800

    gengxin

--- vue/src/components/adIndex.vue
+++ vue/src/components/adIndex.vue
@@ -1,15 +1,35 @@
 <template>
-    <AdNav></AdNav>
+    <el-row>
+        <el-col :span="4">
+            <AdNav :menus='menusArr'></AdNav>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
     import Vue from 'vue'
     import AdNav from './parts/_nav_ad.vue'
 
+    var menus = [
+        { name: '导航1', subMenu: ['1', '11', '111'] },
+        { name: '导航2', subMenu: ['2', '22', '222'] },
+        { name: '导航3'},
+        { name: '导航4', subMenu: ['4', '44'] }
+    ]
+    var menusArr = menus.map((a) => {
+        if(a.subMenu == undefined || a.subMenu.length == 0){
+            a.onlyOne = true;
+        }else{
+            a.onlyOne = false;
+        }
+        return a;
+    })
+
     export default {
         data () {
             return {
-                title: 'admin index'
+                title: 'admin index',
+                menusArr
             }
         },
         components: {
--- vue/src/components/parts/_nav_ad.vue
+++ vue/src/components/parts/_nav_ad.vue
@@ -1,49 +1,34 @@
 <template>
-	<nav id='ad-nav'>
+	<el-row>
+		<el-col :span="4">
+			<el-menu id='ad-nav' default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" v-for="(item, i) in menu">
+				<el-menu-item index="i" v-if="item.onlyOne">
+					<i class="el-icon-setting"></i>{{item.name}}
+				</el-menu-item>
 
-		<el-row>
-			<el-col :span="4">
-				<el-menu default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" v-for="(item, i) in menus">
-
-					<el-submenu index="i" open="1">
-						<el-menu-item index="i" v-if="onlyOne">{{item.name}}</el-menu-item>
-						<template slot="title" v-if="!onlyOne">
-							<i class="el-icon-message"></i>{{item.name}}
-						</template>
-	                    <el-menu-item-group v-if="!onlyOne" v-for="(sub, j) in item.subMenu">
-	                        <el-menu-item index="j">{{sub}}</el-menu-item>
-	                    </el-menu-item-group>
-					</el-submenu>
-					
-				</el-menu>
-			</el-col>
-		</el-row>
-
-	</nav>
+				<el-submenu index="i" open="1" v-if="!item.onlyOne">
+					<template slot="title">
+						<i class="el-icon-message"></i>{{item.name}}
+					</template>
+                    <el-menu-item-group v-for="(sub, j) in item.subMenu">
+                        <el-menu-item index="j">
+                        	<i class="el-icon-message"></i>{{sub}}
+                        </el-menu-item>
+                    </el-menu-item-group>
+				</el-submenu>
+			</el-menu>
+		</el-col>
+	</el-row>
 </template>
 
 
 <script>
-	var menus = [
-		{ name: '导航1', subMenu: ['1', '11', '111'] },
-		{ name: '导航2', subMenu: ['2', '22', '222'] },
-		{ name: '导航3'},
-		{ name: '导航4', subMenu: ['4', '44'] }
-	]
-	var menus1 = menus.map((a) => {
-		if(a.subMenu==undefined || a.subMenu.length==0){
-			a.onlyOne = true;
-		}else{
-			a.onlyOne = false;
-		}
-		return a;
-	})
     export default {
+    	props: ['menus'],
         data () {
             return {
                 title: 'admin index',
-                menus: menus1
-
+                menu: this.menus
             }
         },
         methods : {
@@ -51,14 +36,15 @@
                 console.log(open);
             },
             handleClose(close) {
-                this.$message({
-                    type: 'error',
-                    message: 'close'
-                })
+                // this.$message({
+                //     type: 'error',
+                //     message: 'close'
+                // })
             }
         },
         mounted () {
-
+        	// this.menus = this.props.data;
+        	console.log(this.menu)
         }
     }
 </script>
