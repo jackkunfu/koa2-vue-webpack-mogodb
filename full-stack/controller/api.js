9952381569d0be6101af400fe3fb87149d432090
shanguoqiang <guoqiang.shan@healthbok.com>
2017-08-10 13:45:26 +0800

    mongoose 更新

--- full-stack/controller/api.js
+++ full-stack/controller/api.js
@@ -3,43 +3,88 @@ const api = require('koa-router')();
 api.prefix('/api');
 
 const model = require('../model')
-const PdNav = model.PdNav
+const PdRole = model.PdRole
 
 api.post('/aa', async(ctx, next) => {
     console.log('3333222')
+
     var data = await cnt.getList('hutong')
     console.log('3333222')
     ctx.body = data;
 })
 
+api.post('/admin/list', async(ctx, next) => {
+    var search = await PdRole.fentch({ name: newRole.name })
+    ctx.body = {
+        success: true,
+        msg: '查询成功',
+        data: search
+    };
+})
 api.post('/admin/updateRole', async(ctx, next) => {
     var newRole = ctx.request.body;
     newRole.url = newRole.url || ''
+    newRole.power = newRole.power || []
 
-    PdNav.fentch({ name: newRole.name }, async(err, res) => {
-        err && console.log(err);
-        console.log(res)
 
-        if (res.length == 0) {
-            new PdNav(newRole).save((err, res) => {
-                err && console.log(err);
-                console.log(res)
-                if (!err) {
-                    ctx.body = {
-                        success: true,
-                        msg: '添加角色成功'
-                    };
-                }
-            })
-        } else {
-            var update = await PdNav.updata(res, { name });
-            console.log(res);
+    if (!newRole.id) {
+        var search = await PdRole.fentch({ name: newRole.name })
+
+        if (search.length !== 0) {
             ctx.body = {
+                msg: '已存在该名称的分类',
                 success: false,
-                msg: '已存在该角色'
-            };
+                data: search[0]
+            }
+            return
         }
-    })
+
+        newRole.createTime = new Date()
+        var save = await new PdRole(newRole).save();
+        var all = await PdRole.fentch()
+
+        ctx.body = {
+            data: all,
+            success: true,
+            msg: '添加角色成功'
+        }
+    } else {
+
+        PdRole.fentch({ name: newRole.name }, async(err, res) => {
+            err && console.log(err);
+            console.log(res)
+
+            if (res.length == 0) {
+                newRole.id = null;
+                new PdRole(newRole).save((err, res) => {
+                    err && console.log(err);
+                    console.log(res)
+                    if (!err) {
+                        ctx.body = {
+                            success: true,
+                            msg: '添加角色成功',
+                            data: res
+                        };
+                    }
+                })
+
+            } else {
+
+                var update = await PdRole.updata(res, newRole);
+                console.log(res);
+                ctx.body = {
+                    success: false,
+                    msg: '更新角色成功',
+                    data: res
+                };
+            }
+        })
+
+
+
+    }
+
+
 
 })
 
--- full-stack/model/index.js
+++ full-stack/model/index.js
@@ -2,9 +2,11 @@ var userModel = require('./models/user.js')
 var Movie = require('./models/movie.js')
 
 var PdNav = require('./models/pd/nav.js')
+var PdRole = require('./models/pd/role.js')
 
 module.exports = {
     userModel,
     Movie,
-    PdNav
+    PdNav,
+    PdRole
 }
