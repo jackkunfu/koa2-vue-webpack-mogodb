05c9829a591d16f5e995750e3a7369ba22370238
wadaa <632792338@qq.com>
2017-11-01 23:29:36 +0800

    gengxin

--- full-stack-zw-sports/sports/js/common.js
+++ full-stack-zw-sports/sports/js/common.js
@@ -15,7 +15,7 @@
     }
 
     if ('ontouchstart' in window) {
-        document.documentElement.style.fontSize = document.documentElement.clientWidth / 37.5 + 'px';
+        document.documentElement.style.fontSize = document.documentElement.clientWidth / 30 + 'px';
     }
 
 
