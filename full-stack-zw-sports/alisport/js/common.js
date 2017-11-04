2bd8836a2df9325b28a66e54aecb6e4de44c403c
wadaa <632792338@qq.com>
2017-11-04 10:38:53 +0800

    gegnxin

---
+++ full-stack-zw-sports/alisport/js/common.js
@@ -0,0 +1,22 @@
+(function(Vue, $) {
+    var base = 'http://zhengwusport.com'
+    Vue.prototype._ajaxData = function(type, url, options, cb) {
+        $.ajax({
+            type: type,
+            url: base + url,
+            dataType: 'json',
+            data: options
+        }).done(function(res) {
+			cb(res);
+        }).fail(function(e) {
+            console.error(e);
+            alert('请求失败'+e)
+        })
+    }
+
+    if ('ontouchstart' in window) {
+        document.documentElement.style.fontSize = document.documentElement.clientWidth / 30 + 'px';
+    }
+
+
+})(Vue, $)
