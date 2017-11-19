0b59445fcab527fae183ea5fd55d9118e5b6f65f
wadaa <632792338@qq.com>
2017-11-19 22:57:53 +0800

    gz

--- full-stack-zw-sports/sports/js/common.js
+++ full-stack-zw-sports/sports/js/common.js
@@ -1,17 +1,22 @@
 (function(Vue, $) {
-    var base = 'http://zhengwusport.com'
+    // var base = 'http://zhengwusport.com'
+    var base = ''
     // var base = 'http://zhengwu.com:8888/web';
     Vue.prototype._ajaxData = function(type, url, options, cb) {
         $.ajax({
             type: type,
             url: base + url,
             dataType: 'json',
-            data: options
+            data: options,
+            xhrFields: {
+                withCredentials: true
+            },
+            // crossDomain: true
         }).done(function(res) {
 			cb(res);
         }).fail(function(e) {
             console.error(e);
-            alert('请求失败'+e)
+            alert('请求失败'+JSON.stringify(e))
         })
     }
 
